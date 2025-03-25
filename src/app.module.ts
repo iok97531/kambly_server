addb8a08b13cc0f1e8d76ee697dcc98139a5fb40
Hyoungchul Kim <iok97531@naver.com>
2025-03-25 17:58:34 +0900

    .env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,23 @@ import { VideosModule } from './videos/videos.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { ConfigModule } from '@nestjs/config';
+
+console.log(process.env)
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `src/config/.env`,
+      isGlobal: true,
+    }),
     // Database setup needed
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User],
       synchronize: true,
     }),
